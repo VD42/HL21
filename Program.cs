67134b981c373e67339eb30d6f9306089990a82e
VD42 <vladislav.kochemaev@gmail.com>
2021-02-24 09:11:28 +0400

    Dummy digger and some exceptions catcher

--- Program.cs
+++ Program.cs
@@ -19,129 +19,169 @@ namespace HL21
 
         public async System.Threading.Tasks.Task<Block> post_explore(int posX, int posY, int sizeX, int sizeY)
         {
-            var request_text_stream = new System.IO.MemoryStream();
-            var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
-
-            request_json_stream.WriteStartObject();
-            request_json_stream.WriteNumber("posX", posX);
-            request_json_stream.WriteNumber("posY", posY);
-            request_json_stream.WriteNumber("sizeX", sizeX);
-            request_json_stream.WriteNumber("sizeY", sizeY);
-            request_json_stream.WriteEndObject();
-
-            request_json_stream.Flush();
-
-            var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
-            request.Headers.Add("Content-Type", "application/json");
-
-            var response = await m_http.PostAsync("/explore", request);
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
-                return null;
-
-            var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-            return new Block()
+            try
             {
-                posX = json.RootElement.GetProperty("area").GetProperty("posX").GetInt32(),
-                posY = json.RootElement.GetProperty("area").GetProperty("posY").GetInt32(),
-                sizeX = json.RootElement.GetProperty("area").GetProperty("sizeX").GetInt32(),
-                sizeY = json.RootElement.GetProperty("area").GetProperty("sizeY").GetInt32(),
-                amount = json.RootElement.GetProperty("amount").GetInt32()
-            };
+                var request_text_stream = new System.IO.MemoryStream();
+                var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
+
+                request_json_stream.WriteStartObject();
+                request_json_stream.WriteNumber("posX", posX);
+                request_json_stream.WriteNumber("posY", posY);
+                request_json_stream.WriteNumber("sizeX", sizeX);
+                request_json_stream.WriteNumber("sizeY", sizeY);
+                request_json_stream.WriteEndObject();
+
+                request_json_stream.Flush();
+
+                var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
+                request.Headers.Add("Content-Type", "application/json");
+
+                var response = await m_http.PostAsync("/explore", request);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    return null;
+
+                var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                return new Block()
+                {
+                    posX = json.RootElement.GetProperty("area").GetProperty("posX").GetInt32(),
+                    posY = json.RootElement.GetProperty("area").GetProperty("posY").GetInt32(),
+                    sizeX = json.RootElement.GetProperty("area").GetProperty("sizeX").GetInt32(),
+                    sizeY = json.RootElement.GetProperty("area").GetProperty("sizeY").GetInt32(),
+                    amount = json.RootElement.GetProperty("amount").GetInt32()
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("[" + System.DateTime.Now.ToString() + "]");
+                Console.Error.WriteLine(ex.ToString());
+                Console.Error.WriteLine();
+                return null;
+            }
         }
 
         public async System.Threading.Tasks.Task<License> post_license(int coin = -1)
         {
-            var request_text_stream = new System.IO.MemoryStream();
-            var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
-
-            request_json_stream.WriteStartArray();
-            if (coin != -1)
-                request_json_stream.WriteNumberValue(coin);
-            request_json_stream.WriteEndArray();
-
-            request_json_stream.Flush();
-
-            var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
-            request.Headers.Add("Content-Type", "application/json");
-
-            var response = await m_http.PostAsync("/licenses", request);
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
-                return null;
-
-            var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-            return new License()
+            try
             {
-                id = json.RootElement.GetProperty("id").GetInt32(),
-                digAllowed = json.RootElement.GetProperty("digAllowed").GetInt32(),
-                digUsed = json.RootElement.GetProperty("digUsed").GetInt32()
-            };
+                var request_text_stream = new System.IO.MemoryStream();
+                var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
+
+                request_json_stream.WriteStartArray();
+                if (coin != -1)
+                    request_json_stream.WriteNumberValue(coin);
+                request_json_stream.WriteEndArray();
+
+                request_json_stream.Flush();
+
+                var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
+                request.Headers.Add("Content-Type", "application/json");
+
+                var response = await m_http.PostAsync("/licenses", request);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    return null;
+
+                var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                return new License()
+                {
+                    id = json.RootElement.GetProperty("id").GetInt32(),
+                    digAllowed = json.RootElement.GetProperty("digAllowed").GetInt32(),
+                    digUsed = json.RootElement.GetProperty("digUsed").GetInt32()
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("[" + System.DateTime.Now.ToString() + "]");
+                Console.Error.WriteLine(ex.ToString());
+                Console.Error.WriteLine();
+                return null;
+            }
         }
 
         public async System.Threading.Tasks.Task<System.Collections.Generic.List<string>> post_dig(int licenseID, int posX, int posY, int depth)
         {
-            var request_text_stream = new System.IO.MemoryStream();
-            var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
-
-            request_json_stream.WriteStartObject();
-            request_json_stream.WriteNumber("licenseID", licenseID);
-            request_json_stream.WriteNumber("posX", posX);
-            request_json_stream.WriteNumber("posY", posY);
-            request_json_stream.WriteNumber("depth", depth);
-            request_json_stream.WriteEndObject();
-
-            request_json_stream.Flush();
-
-            var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
-            request.Headers.Add("Content-Type", "application/json");
-
-            var response = await m_http.PostAsync("/dig", request);
-            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                return new System.Collections.Generic.List<string>();
-            if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+            try
             {
-                var kostyl = new System.Collections.Generic.List<string>();
-                kostyl.Add("i_need_license!!!");
-                return kostyl;
+                var request_text_stream = new System.IO.MemoryStream();
+                var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
+
+                request_json_stream.WriteStartObject();
+                request_json_stream.WriteNumber("licenseID", licenseID);
+                request_json_stream.WriteNumber("posX", posX);
+                request_json_stream.WriteNumber("posY", posY);
+                request_json_stream.WriteNumber("depth", depth);
+                request_json_stream.WriteEndObject();
+
+                request_json_stream.Flush();
+
+                var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
+                request.Headers.Add("Content-Type", "application/json");
+
+                var response = await m_http.PostAsync("/dig", request);
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    return new System.Collections.Generic.List<string>();
+                if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                {
+                    var kostyl = new System.Collections.Generic.List<string>();
+                    kostyl.Add("i_need_license!!!");
+                    return kostyl;
+                }
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    return null;
+
+                var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+
+                var length = json.RootElement.GetArrayLength();
+                var treasures = new System.Collections.Generic.List<string>(length);
+
+                for (int i = 0; i < length; ++i)
+                    treasures.Add(json.RootElement[i].GetString());
+
+                return treasures;
             }
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("[" + System.DateTime.Now.ToString() + "]");
+                Console.Error.WriteLine(ex.ToString());
+                Console.Error.WriteLine();
                 return null;
-
-            var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-
-            var length = json.RootElement.GetArrayLength();
-            var treasures = new System.Collections.Generic.List<string>(length);
-
-            for (int i = 0; i < length; ++i)
-                treasures.Add(json.RootElement[i].GetString());
-
-            return treasures;
+            }
         }
 
         public async System.Threading.Tasks.Task<System.Collections.Generic.List<int>> post_cash(string treasure)
         {
-            var request_text_stream = new System.IO.MemoryStream();
-            var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
+            try
+            {
+                var request_text_stream = new System.IO.MemoryStream();
+                var request_json_stream = new System.Text.Json.Utf8JsonWriter(request_text_stream);
 
-            request_json_stream.WriteStringValue(treasure);
+                request_json_stream.WriteStringValue(treasure);
 
-            request_json_stream.Flush();
+                request_json_stream.Flush();
 
-            var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
-            request.Headers.Add("Content-Type", "application/json");
+                var request = new System.Net.Http.ByteArrayContent(request_text_stream.ToArray());
+                request.Headers.Add("Content-Type", "application/json");
 
-            var response = await m_http.PostAsync("/cash", request);
-            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                var response = await m_http.PostAsync("/cash", request);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                    return null;
+
+                var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+
+                var length = json.RootElement.GetArrayLength();
+                var money = new System.Collections.Generic.List<int>(length);
+
+                for (int i = 0; i < length; ++i)
+                    money.Add(json.RootElement[i].GetInt32());
+
+                return money;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("[" + System.DateTime.Now.ToString() + "]");
+                Console.Error.WriteLine(ex.ToString());
+                Console.Error.WriteLine();
                 return null;
-
-            var json = System.Text.Json.JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-
-            var length = json.RootElement.GetArrayLength();
-            var money = new System.Collections.Generic.List<int>(length);
-
-            for (int i = 0; i < length; ++i)
-                money.Add(json.RootElement[i].GetInt32());
-
-            return money;
+            }
         }
 
         public async System.Threading.Tasks.Task explore_blocks(int posX, System.Collections.Generic.List<Block> blocks)
@@ -232,23 +272,26 @@ namespace HL21
             int port = int.Parse(System.Environment.GetEnvironmentVariable("Port") ?? "8000");
             string schema = System.Environment.GetEnvironmentVariable("Schema") ?? "http";
 
-            var max_clients = 100;
+            var max_clients = 10;
             var max_dig_clients = 10;
 
             var clients = new System.Collections.Generic.List<Client>(max_clients);
             for (int i = 0; i < max_clients; ++i)
                 clients.Add(new Client(schema, host, port));
 
-            var blocks = new System.Collections.Generic.List<Block>(1000);
+            var blocks = new System.Collections.Generic.List<Block>(10);
 
             // Explore
             // 100x100 blocks, 35x35 size
             // Each client tests 100 vertical blocks and 2 horizontal
-            var explore_tasks = new System.Collections.Generic.List<System.Threading.Tasks.Task>(100);
+            /*var explore_tasks = new System.Collections.Generic.List<System.Threading.Tasks.Task>(100);
             for (int i = 0; i < 100; ++i)
                 explore_tasks.Add(clients[i % max_clients].explore_blocks(i * 35, blocks));
             System.Threading.Tasks.Task.WhenAll(explore_tasks).Wait();
-            blocks.Sort();
+            blocks.Sort();*/
+
+            for (int i = 0; i < 10; ++i)
+                blocks.Add(new Block { posX = i * 350, posY = 0, sizeX = 350, sizeY = 3500, amount = 0 });
 
             var coins = new System.Collections.Generic.List<int>(1000000);
 
