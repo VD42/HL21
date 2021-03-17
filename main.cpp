473ed027ebedeb8ce46ff3971b1f7d9a1c2f6d85
VD42 <vladislav.kochemaev@gmail.com>
2021-03-17 20:45:05 +0400

    Best score

--- main.cpp
+++ main.cpp
@@ -359,7 +359,7 @@ public:
         }));
 
         if (coins.size() == 0)
-            curl_easy_setopt(curl.get(), CURLOPT_TIMEOUT_MS, 200L);
+            curl_easy_setopt(curl.get(), CURLOPT_TIMEOUT_MS, 150L);
 
         const auto curl_code = curl_easy_perform(curl.get());
 
@@ -833,7 +833,7 @@ bool CLicenseManager::update_licenses(CClient& client)
     if (!working)
         return false;
 
-    const auto use_free = (std::rand() % 10 < 2);
+    const auto use_free = (std::rand() % 10 < 3);
 
     std::optional<CLicense> license;
     while (!license.has_value())
